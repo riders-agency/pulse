b81d1939376187252545989e62b7dbcf1b2077c0
Sergei <cto@studiointer.net>
2023-11-07 18:12:07 +0300

    Change composer

--- src/PulseClient.php
+++ src/PulseClient.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace RidersAgency;
-
 class PulseClient {
     public function send($uuid, $data) {
         $client = new \GuzzleHttp\Client();
-
         $response = $client->request('POST', 'https://pulse.riders.agency/send', [
             'query' => [
                 'uuid' => $uuid,
                 'data' => json_encode($data),
             ]
         ]);
-
         return $response;
     }
 }
