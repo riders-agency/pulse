41e47fd48bbf46ebb099a4d34af2eaff592cd2a0
Sergei <cto@studiointer.net>
2023-11-07 13:38:35 +0300

    Initial

---
+++ src/RA/PulseClient.php
@@ -0,0 +1,19 @@
+namespace RA;
+
+class PulseClient {
+    public function send($uuid, $data) {
+        $client = new \GuzzleHttp\Client();
+
+        $response = $client->request('POST', 'https://pulse.riders.agency/send', [
+            'query' => [
+                'uuid' => $uuid,
+                'data' => json_encode($data),
+            ]
+        ]);
+
+        // Process the response as needed
+        // ...
+
+        return $response;
+    }
+}
